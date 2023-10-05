c169503f4ee5167f4a83385796bcb597d88cb683
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-10-05 22:12:06 +0200

    formatting

--- fuzz/main.go
+++ fuzz/main.go
@@ -15,14 +15,13 @@ func main() {
 	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
 }
 
-
 func Reverse(s string) (string, error) {
 	if !utf8.ValidString(s) {
 		return s, errors.New("input is not valid UTF-8")
-}
-r := []rune(s)
-for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+	}
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
-}
-return string(r), nil
+	}
+	return string(r), nil
 }
