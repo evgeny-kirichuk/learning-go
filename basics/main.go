9f96cd5335e8d2811474de058d623a1e3aad422d
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-10-08 13:17:26 +0200

    started a module refactoring. separated fuzz module to a package

--- basics/main.go
+++ basics/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"learning/basics/fuzz"
+)
+
 func main() {
 	println("Hello, world")
+
+	reversed, err := fuzz.Reverse("Hello, world")
+	fmt.Print(reversed, err)
 }
