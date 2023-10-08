b7cd8b950593db001c6c14fcab6b950593ad997c
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-10-08 21:26:20 +0200

    add concurrency go routines

--- basics/moreofgo/maps.go
+++ basics/moreofgo/maps.go
@@ -12,22 +12,19 @@ type User struct {
 }
 
 func maps() {
-	var count uint = 10
-	userData := map[string]User{}
-	userData["John"] = User{"John", "Doe", count}
-	userData["Adam"] = User{"Adam", "Doe", count}
-	fmt.Println(userData, &count)
-
-	for key, user := range userData {
-		sendMessage("Hello, " + user.firstName + " " + user.lastName + " (" + key + ")");
+	var username string
+	for {
+		fmt.Println("Enter your name: ")
+		fmt.Scan(&username)
+		go sendMessage("Hello, " + username);
 	}
 }
 
 func sendMessage(text string) {
+	time.Sleep(5 * time.Second)
 	message := fmt.Sprintf("New message: %v \n", text)
 	fmt.Println("###########")
 	fmt.Printf("sending message: %v",message)
 	fmt.Println("###########")
 
-	time.Sleep(5 * time.Second)
 }
