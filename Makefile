45eaa5c05df72c9c2649f239a5f4839a219a1c61
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-10-12 20:43:12 +0200

    add executable example

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ basics:
 
 booking:
 		@go run ./cmd/bookingApi/main.go
+
+buildBooking:
+		@go build -o ./bin/bookingApi ./cmd/bookingApi/main.go
+
+testBooking:
+		@go test -v ./cmd/bookingApi/...
--- internal/bookingApi/api.go
+++ internal/bookingApi/api.go
@@ -4,5 +4,10 @@ import "github.com/gofiber/fiber"
 
 func StartServer() {
 	app := fiber.New()
+	app.Get("/bookings", handleBookings)
 	app.Listen(":3000")
+}
+
+func handleBookings(c *fiber.Ctx) {
+	c.JSON(map[string]string{"booking": "booked"})
 }
