2f194c19dc825e3c8dcfad2a58a04d1e9e1c5526
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-10-13 08:23:32 +0200

    add user type

--- internal/bookingApi/handlers/user_handler.go
+++ internal/bookingApi/handlers/user_handler.go
@@ -1,11 +1,18 @@
 package handlers
 
 import (
+	"learning_go/internal/bookingApi/types"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func HandleGetUsers(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"user": "Evgeny Kirichuk"})
+	u := types.User{
+		FirstName: "Evgeny",
+		LastName:  "Kirichuk",
+		Age:       30,
+	}
+	return c.JSON(u)
 }
 
 func HandleGetUserById(c *fiber.Ctx) error {
---
+++ internal/bookingApi/types/user.go
@@ -0,0 +1,8 @@
+package types
+
+type User struct {
+	ID        string `bson:"id" json:"id"` // bson - binary json. use ,omitempty to ignore empty fields
+	FirstName string `bson:"firstName" json:"firstName"`
+	LastName  string `bson:"lastName" json:"lastName"`
+	Age       int    `bson:"age" json:"age"`
+}
