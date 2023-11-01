59e7502d26788042123d65e2915371d70b761c1e
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-11-01 10:19:53 +0100

    refactor seed script structure

--- scripts/seed.go
+++ scripts/seed.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"learning_go/bookingApi/db"
 	"learning_go/bookingApi/types"
 	"log"
@@ -12,45 +11,68 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var (
+	client *mongo.Client
+	roomStore db.RoomStore
+	hotelStore db.HotelStore
+	ctx = context.Background()
+)
+
+func seedHotel(name, address string) {
+	hotel := types.Hotel{
+		Name:    name,
+		Address: address,
+		Rooms: []primitive.ObjectID{},
+	}
+
+	rooms := []types.Room{
+		{
+			Type:      types.Single,
+			BasePrice: 100,
+			Price:     100,
+		},
+		{
+			Type:      types.Double,
+			BasePrice: 200,
+			Price:     200,
+		},
+		{
+			Type:      types.Single,
+			BasePrice: 300,
+			Price:     300,
+		},
+	}
+
+		insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, room := range rooms {
+			room.HotelID = insertedHotel.ID
+			insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+			if err != nil {
+				log.Fatal(err)
+			}
+			insertedHotel.Rooms = append(insertedHotel.Rooms, insertedRoom.ID)
+		}
+}
+
 func main() {
-	ctx := context.Background()
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBuri))
+	defer client.Disconnect(context.Background())
+	seedHotel("Hilton", "1234 Main Street")
+}
+
+func init() {
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBuri))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer client.Disconnect(context.Background())
 
 	if err := client.Database(db.DBname).Drop(ctx); err != nil {
 		log.Fatal(err)
 	}
 
-	hotelStore := db.NewMongoHotelStore(client, db.DBname)
-	roomStore := db.NewMongoRoomStore(client, db.DBname, hotelStore)
-
-	hotel := types.Hotel{
-		Name:    "Hilton",
-		Address: "1234 Main St",
-		Rooms: []primitive.ObjectID{},
-	}
-
-	room := types.Room{
-		Type:      types.Single,
-		BasePrice: 100,
-		Price:     100,
-	}
-
-	indertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	room.HotelID = indertedHotel.ID
-
-	insertedRoom, err := roomStore.InsertRoom(ctx, &room)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-
-	fmt.Println(indertedHotel, insertedRoom)
+	hotelStore = db.NewMongoHotelStore(client, db.DBname)
+	roomStore = db.NewMongoRoomStore(client, db.DBname, hotelStore)
 }
