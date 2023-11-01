5125359f1b0e082757848474b621eacc722b3ae8
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-11-01 11:09:05 +0100

    add query params parsing

--- bookingApi/handlers/hotel_handler.go
+++ bookingApi/handlers/hotel_handler.go
@@ -15,7 +15,15 @@ func NewHotelHandler(rs db.RoomStore, hs db.HotelStore) *HotelHandler {
 	return &HotelHandler{roomStore: rs, hotelStore: hs}
 }
 
+type HotelQuery struct {
+	Rooms bool
+}
+
 func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
+	var query HotelQuery
+	if err := c.QueryParser(&query); err != nil {
+		return err
+	}
 	hotels, err := h.hotelStore.List(c.Context(), nil)
 	if err != nil {
 		return err
